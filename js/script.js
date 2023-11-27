9f9a21ae0e916a9b895571dbd45fe6684bcb5223
Toni Toyobo <tonitoyobo@gmail.com>
2023-11-27 02:48:29 -0500

    feat Toni: table.html has table stuff (for onyi)

--- js/script.js
+++ js/script.js
@@ -3,9 +3,6 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
-const searchForm = document.querySelector('form');
-const searchInput = document.querySelector('#search');
-const resultsList  = document.querySelector('#results');
 
 //listeners for login and register
 registerLink.addEventListener('click', ()=> {
@@ -31,22 +28,13 @@ searchForm.addEventListener('submit', (e) => {
 })
 
 
-function changePage(page) {
-    // You can load content dynamically based on the page parameter
-    // For simplicity, let's just update the content div with a simple message
-    var contentDiv = document.getElementById('content');
-    
-    switch (page) {
-        case 'home':
-            contentDiv.innerHTML = '<h2>Welcome to the Home Page!</h2>';
-            break;
-        case 'about':
-            contentDiv.innerHTML = '<h2>About Us</h2><p>We are a fantastic company!</p>';
-            break;
-        case 'contact':
-            contentDiv.innerHTML = '<h2>Contact Us</h2><p>Reach out to us at contact@example.com</p>';
-            break;
-        default:
-            contentDiv.innerHTML = '<h2>Page not found</h2>';
-    }
-}
+//star ratings
+const stars = document.querySelectorAll(".stars ion-icon");
+
+//loop through stars NodeList
+stars.forEach((star, index1) => {
+    //event listener for when click event is triggered
+    star.addEventListener("click", () => {
+        console.log(clicked);
+    });
+});
--- templates/script.js
+++ templates/script.js
@@ -1,11 +1,9 @@
+//login and register starts
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
-const searchForm = document.querySelector('form');
-const searchInput = document.querySelector('#search');
-const resultsList  = document.querySelector('#results');
 
 //listeners for login and register
 registerLink.addEventListener('click', ()=> {
@@ -25,21 +23,20 @@ iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
 
-//listener for search
-searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-})
+//login and register ends
 
-//asynchronous request to server
-fetch('/api/data')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('HTTP error! Status: ${response.status}');
-        }
-        return response.json();
-    })
-    .then(data => {
-        //update html with data from server
-        document.getElementById('result').innerText = data.message;
-    })
-    .catch(error => console.error('Error:', error));
+//star ratings
+const stars = document.querySelectorAll(".stars ion-icon");
+
+//loop through stars NodeList
+stars.forEach((star, index1) => {
+    //event listener for when click event is triggered
+    star.addEventListener("click", () => {
+        //loop through the "stars" Nodelist again
+        stars.forEach((star, index2) => {
+            //add "active" class to the clicked star and any stars with
+            //a lower index and remove the "active" class from any stars with a higher index
+            index1 >= index2 ? star.classList.add("active") : star.classList.remove("active")
+        });
+    });
+});
